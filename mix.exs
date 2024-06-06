d2d2b2f8828379827049dd984844912a315db6d8
Bruno Ribeiro <brf147@gmail.com>
2024-06-06 00:18:54 -0400

    feat: add tesla to the project

--- mix.exs
+++ mix.exs
@@ -50,7 +50,10 @@ defmodule FoodVoting.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:sqids, "~> 0.1.0"},
+      {:tesla, "~> 1.9"},
+      {:hackney, "~> 1.20"}
     ]
   end
 
